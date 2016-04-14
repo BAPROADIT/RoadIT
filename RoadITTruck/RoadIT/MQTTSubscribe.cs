1a3619447448be92d3dc4f02b29693521c392c11
KlaasDeNys <klaas.denys@outlook.com>
2016-04-14 14:10:05 +0200

    Truck start

---
+++ RoadITTruck/RoadIT/MQTTSubscribe.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Java.Lang;
+using Android.Util;
+using Org.Eclipse.Paho.Client.Mqttv3;
+
+namespace RoadIT
+{
+	[Activity(Label = "MqttSubscribe")]
+	public class MqttSubscribe : Activity, IMqttCallback
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			Log.Debug("MqttSubscribe", "create");
+			// Create your application here
+		}
+		public void MessageArrived(string topic, MqttMessage message)
+		{
+			Log.Debug("MqttSubscribe", message.ToString());
+			string test = message.ToString();
+			Finisher.MQTTin(test);
+		}
+
+		public void ConnectionLost(Throwable cause)
+		{
+			Log.Debug("MqttSubscribe", "connectionlost");
+			Finisher.ConfigMQTT();
+		}
+
+		public void DeliveryComplete(IMqttDeliveryToken token)
+		{
+			Log.Debug("MqttSubscribe", "deliverycomplete");
+		}
+
+	}
+}
