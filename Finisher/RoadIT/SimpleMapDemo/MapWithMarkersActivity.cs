1ea9080661fc1207061f947f325b72a14e0a9a76
matsdemeyer <matsdemeyer@gmail.com>
2016-04-28 20:22:33 +0200

    Revert "testproject weg"
    This reverts commit a6b317de31eb51847b5d9acc539f930ce70791fa.

---
+++ Finisher/RoadIT/SimpleMapDemo/MapWithMarkersActivity.cs
@@ -0,0 +1,396 @@
+namespace ROADIT
+{
+    using Android.App;
+    using Android.Gms.Maps;
+    using Android.Gms.Maps.Model;
+    using Android.OS;
+	using Android.Locations;
+	using Android.Util;
+    using Android.Widget;
+	using Android.Content;
+	using Android.Runtime;
+	using Android.Views;
+	using System.Json;
+	using System;
+	using System.Threading;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using Newtonsoft.Json;
+	using Newtonsoft.Json.Linq;
+	//using Java.Lang;
+
+	using Org.Eclipse.Paho.Client.Mqttv3;
+	using Org.Eclipse.Paho.Client.Mqttv3.Internal;
+	using Org.Eclipse.Paho.Client.Mqttv3.Logging;
+	using Org.Eclipse.Paho.Client.Mqttv3.Persist;
+	using Org.Eclipse.Paho.Client.Mqttv3.Util;
+
+	[Activity(Label = "ROAD IT", MainLauncher = true)]
+
+	public class MapWithMarkersActivity : Activity, ILocationListener
+	{
+		private static readonly LatLng truck1loc = new LatLng(51.229241, 4.404648);
+		private static LatLng cineloc = new LatLng(51.2354242, 4.4105663);
+		private LatLng finisherloc = new LatLng(0, 0);
+		private GoogleMap map;
+		private MapFragment mapFragment;
+		private LocationManager locMgr;
+		string ownlocstring;
+		string truckstring;
+		string cinestring;
+		static string varloc="";
+		string durationString;
+		private JObject _Jobj;
+		string tag = "MainActivity";
+
+		MarkerOptions markerfinisher = new MarkerOptions();
+		MarkerOptions markertruck = new MarkerOptions();
+		public static string broker = "tcp://iot.eclipse.org:1883";
+		public static string clientId = "JavaSample";
+
+		public static MemoryPersistence persistence = new MemoryPersistence();
+		public static MqttClient Client = new MqttClient(broker, clientId, persistence);
+
+		bool firstloc = true;
+
+		public void OnLocationChanged(Android.Locations.Location location)
+		{
+			Toast.MakeText(this, "Location changed", ToastLength.Long).Show();
+			finisherloc = new LatLng(location.Latitude, location.Longitude);
+			if (firstloc == true)
+			{
+				InitMarkers();
+				ZoomOnLoc();
+				locsToString();
+				firstloc = false;
+			}
+			locsToString();
+			RefreshMarkers();
+
+			//Thread MapsAPICallThread = new Thread(() => mapAPICall(ownlocstring,truckstring));
+			//MapsAPICallThread.Start();
+
+
+			//multithreaded method call, prevents app stutters
+			ThreadStart getDurationThreadStart = new ThreadStart(getDuration);
+			Thread getDurationThread = new Thread(getDurationThreadStart);
+			getDurationThread.Start();
+
+			//ThreadStart drawRouteThreadStart = new ThreadStart(drawRoute(ownlocstring,truckstring));
+			Thread drawRouteThread = new Thread(() => drawRoute(truckstring, "red"));
+			drawRouteThread.Start();
+
+		}
+
+		public static void MQTTin(string mqttin)
+		{
+			Log.Debug("MQTTEST", mqttin);
+			varloc = mqttin;
+		}
+	
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+			Log.Debug (tag, "OnCreate called");		
+            SetContentView(Resource.Layout.MapLayout);
+            InitMapFragment();
+			SetupAnimateToButton();
+			ConfigMQTT();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+			Log.Debug (tag, "OnResume called");
+
+			// initialize location manager
+			locMgr = GetSystemService (Context.LocationService) as LocationManager;
+
+			// pass in the provider (GPS),
+			// the minimum time between updates (in seconds),
+			// the minimum distance the user needs to move to generate an update (in meters),
+			// and an ILocationListener (recall that this class impletents the ILocationListener interface)
+			if (locMgr.AllProviders.Contains (LocationManager.NetworkProvider)
+				&& locMgr.IsProviderEnabled (LocationManager.NetworkProvider)) {
+ 				locMgr.RequestLocationUpdates (LocationManager.NetworkProvider, 2000, 1, this);
+			} else {
+				Toast.MakeText (this, "The Network Provider does not exist or is not enabled!", ToastLength.Long).Show ();
+			}
+        }
+
+		void ConfigMQTT()
+		{
+			try
+			{
+				Client.SetCallback(new MqttSubscribe());
+				Client.Connect();
+
+				Client.Subscribe("fin");
+				Toast.MakeText(this, "Subscribe(\"fin\")!", ToastLength.Long).Show();
+
+			}
+			catch (MqttException me)
+			{
+				Toast.MakeText(this, "Error: Subscribe(\"fin\")!\n" + me, ToastLength.Long).Show();
+
+			}
+		}
+
+
+		protected override void OnStart ()
+		{
+			base.OnStart ();
+			Log.Debug (tag, "OnStart called");
+		}
+
+        private void InitMapFragment()
+        {
+            mapFragment = FragmentManager.FindFragmentByTag("map") as MapFragment;
+
+            if (mapFragment == null)
+            {
+                GoogleMapOptions mapOptions = new GoogleMapOptions()
+					.InvokeMapType(GoogleMap.MapTypeNormal)
+					.InvokeZoomControlsEnabled(true)
+					.InvokeCompassEnabled(true);
+				
+                FragmentTransaction fragTx = FragmentManager.BeginTransaction();
+                mapFragment = MapFragment.NewInstance(mapOptions);
+                fragTx.Add(Resource.Id.map, mapFragment, "map");
+                fragTx.Commit();
+            }
+				
+        }
+			
+        private void SetupAnimateToButton()
+        {
+            Button animateButton = FindViewById<Button>(Resource.Id.animateButton);
+            animateButton.Click += (sender, e) =>{
+				Thread drawRouteThread2 = new Thread(() => drawRoute(varloc, "blue"));
+				drawRouteThread2.Start();
+			};
+        }
+
+		//niet meer nodig
+		private void ZoomOnLoc()
+		{
+			CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+			builder.Target(finisherloc);
+			builder.Zoom(12);
+			builder.Bearing(0);
+			builder.Tilt(0);
+			CameraPosition cameraPosition = builder.Build();
+
+			// AnimateCamera provides a smooth, animation effect while moving
+			// the camera to the the position.
+			map.AnimateCamera(CameraUpdateFactory.NewCameraPosition(cameraPosition));
+		}
+
+		private void InitMarkers()
+		{
+			map = mapFragment.Map;
+			BitmapDescriptor truck = BitmapDescriptorFactory.FromResource(Resource.Drawable.truck);
+			markertruck.SetPosition(truck1loc);
+			markertruck.SetTitle("Truck");
+			markertruck.SetIcon(truck);
+			map.AddMarker(markertruck);
+
+			//blue location
+			map.MyLocationEnabled = true;
+		}
+
+		private void RefreshMarkers()
+        {
+			map.Clear ();
+			markertruck.SetPosition(truck1loc);
+			map.AddMarker(markertruck);
+        }
+
+		public void OnProviderDisabled (string provider)
+		{
+			Log.Debug (tag, provider + " disabled by user");
+		}
+		public void OnProviderEnabled (string provider)
+		{
+			Log.Debug (tag, provider + " enabled by user");
+		}
+		public void OnStatusChanged (string provider, Availability status, Bundle extras)
+		{
+			Log.Debug (tag, provider + " availability has changed to " + status.ToString());
+		}
+
+		private void locsToString()
+		{
+			ownlocstring = finisherloc.Latitude.ToString() + "," + finisherloc.Longitude.ToString();
+			truckstring = truck1loc.Latitude.ToString() + "," + truck1loc.Longitude.ToString();
+			cinestring = cineloc.Latitude.ToString() + "," + cineloc.Longitude.ToString();
+		}
+
+		private void getDuration()
+		{
+			//animateButton.Text = "Duration: " + getDistanceTo(ownlocstring,truckstring);
+			durationString = "Duration from truck to finisher: " + getDistanceTo(ownlocstring, truckstring) + "s";
+
+			TextView durationtextfield = FindViewById<TextView>(Resource.Id.textView1);
+
+			//update textfield in main UI thread
+			RunOnUiThread(() => durationtextfield.Text = durationString);
+		}
+
+		public int getDistanceTo(string origin, string destination)
+		{
+			System.Threading.Thread.Sleep(50);
+
+			int duration = -1;
+			string url = "http://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&sensor=false";
+			string requesturl = url; string content = fileGetJSON(requesturl);
+			_Jobj = JObject.Parse(content);
+			try
+			{
+				duration = (int)_Jobj.SelectToken("routes[0].legs[0].duration.value");
+				return duration;
+			}
+			catch
+			{
+				return duration;
+			}
+		}
+
+		private void mapAPICall(string origin, string destination)
+		{
+			System.Threading.Thread.Sleep(50);
+
+			string url = "http://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&sensor=false";
+			string requesturl = url; string content = fileGetJSON(requesturl);
+			_Jobj = JObject.Parse(content);
+
+			getDuration();
+			//drawRoute(ownlocstring,truckstring);
+		}
+
+		private void drawRoute(string origin, string color)
+		{
+			System.Threading.Thread.Sleep(50);
+
+			var polylineOptions = new PolylineOptions();
+			if (color == "blue")
+			{
+				polylineOptions.InvokeColor(0x66000099);
+			}
+			else if (color == "red")
+			{
+				polylineOptions.InvokeColor(0x66ff0000);
+			}
+			else
+			{
+				polylineOptions.InvokeColor(0x66000099);
+			}
+
+			polylineOptions.InvokeWidth(9);
+
+			string url = "http://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + ownlocstring + "&sensor=false";
+			string requesturl = url; string content = fileGetJSON(requesturl);
+			JObject _Jobjdraw = JObject.Parse(content);
+			string polyPoints;
+			polyPoints = (string)_Jobjdraw.SelectToken("routes[0].overview_polyline.points");
+
+			List<LatLng> drawCoordinates;
+			drawCoordinates = DecodePolylinePoints(polyPoints);
+			foreach (var position in drawCoordinates)
+			{
+				polylineOptions.Add(new LatLng(position.Latitude, position.Longitude));
+			}
+
+			//draw route in main UI thread
+			RunOnUiThread(() => map.AddPolyline(polylineOptions));
+
+		}
+
+		private List<LatLng> DecodePolylinePoints(string encodedPoints)
+		{
+			if (encodedPoints == null || encodedPoints == "") return null;
+			List<LatLng> poly = new List<LatLng>();
+			char[] polylinechars = encodedPoints.ToCharArray();
+			int index = 0;
+
+			int currentLat = 0;
+			int currentLng = 0;
+			int next5bits;
+			int sum;
+			int shifter;
+
+			try
+			{
+				while (index < polylinechars.Length)
+				{
+					// calculate next latitude
+					sum = 0;
+					shifter = 0;
+					do
+					{
+						next5bits = (int)polylinechars[index++] - 63;
+						sum |= (next5bits & 31) << shifter;
+						shifter += 5;
+					} while (next5bits >= 32 && index < polylinechars.Length);
+
+					if (index >= polylinechars.Length)
+						break;
+
+					currentLat += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+
+					//calculate next longitude
+					sum = 0;
+					shifter = 0;
+					do
+					{
+						next5bits = (int)polylinechars[index++] - 63;
+						sum |= (next5bits & 31) << shifter;
+						shifter += 5;
+					} while (next5bits >= 32 && index < polylinechars.Length);
+
+					if (index >= polylinechars.Length && next5bits >= 32)
+						break;
+
+					currentLng += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+
+					double latdouble = Convert.ToDouble(currentLat) / 100000.0;
+					double lngdouble = Convert.ToDouble(currentLng) / 100000.0;
+					LatLng p = new LatLng(latdouble,lngdouble);
+					poly.Add(p);
+				}
+			}
+			catch (Exception ex)
+			{
+				// logo it
+			}
+			return poly;
+		}
+
+		protected string fileGetJSON(string fileName)
+		{
+			string _sData = string.Empty;
+			string me = string.Empty;
+			try
+			{
+				if (fileName.ToLower().IndexOf("http:") > -1)
+				{
+					System.Net.WebClient wc = new System.Net.WebClient();
+					byte[] response = wc.DownloadData(fileName);
+					_sData = System.Text.Encoding.ASCII.GetString(response);
+
+				}
+				else
+				{
+					System.IO.StreamReader sr = new System.IO.StreamReader(fileName);
+					_sData = sr.ReadToEnd();
+					sr.Close();
+				}
+			}
+			catch { _sData = "unable to connect to server "; }
+			return _sData;
+		}
+	}
+}
+
---
+++ Finisher/RoadIT/SimpleMapDemo/Resources/Values/MainActivity.cs
@@ -0,0 +1,243 @@
+﻿namespace ROADIT
+{
+    using System.Collections.Generic;
+
+    using Android.App;
+    using Android.Content;
+    using Android.Gms.Common;
+    using Android.OS;
+    using Android.Util;
+    using Android.Views;
+    using Android.Widget;
+    using AndroidUri = Android.Net.Uri;
+	using Android.Hardware;
+	using Android.Locations;
+	using System;
+	//using Android.Support.Design.Widget;
+	using Android.Support.V4.App;
+	using Android.Content.PM;
+
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : ListActivity
+    {
+        public static readonly int InstallGooglePlayServicesId = 1000;
+        public static readonly string Tag = "ROAD IT";
+
+        //private List<SampleActivity> _activities;
+        private bool _isGooglePlayServicesInstalled;
+
+		static readonly int REQUEST_COARSELOCATION = 0;
+		static readonly int REQUEST_FINELOCATION = 1;
+		static readonly int REQUEST_INTERNET = 2;
+
+		static string[] PERMISSIONS_CONTACT = {
+
+			Android.Manifest.Permission.Internet,
+			Android.Manifest.Permission.AccessCoarseLocation,
+			Android.Manifest.Permission.AccessFineLocation,
+		};
+
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            switch (resultCode)
+            {
+                case Result.Ok:
+                    // Try again.
+                    _isGooglePlayServicesInstalled = true;
+                    break;
+
+                default:
+                    Log.Debug("MainActivity", "Unknown resultCode {0} for request {1}", resultCode, requestCode);
+                    break;
+            }
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            _isGooglePlayServicesInstalled = TestIfGooglePlayServicesIsInstalled();
+			//gps = new GPS((LocationManager)GetSystemService(LocationService), _gpsText);
+            initLocationManager();
+			RequestInternetPermission();
+
+			//InitializeListView();
+			SampleActivity activity = new SampleActivity(Resource.String.activity_label_mapwithmarkers, Resource.String.activity_description_mapwithmarkers, typeof(MapWithMarkersActivity));
+			activity.Start(this);
+        }
+
+//        protected override void OnListItemClick(ListView l, View v, int position, long id)
+//        {
+//            if (position == 0)
+//            {
+//                AndroidUri geoUri = AndroidUri.Parse("geo:42.374260,-71.120824");
+//                Intent mapIntent = new Intent(Intent.ActionView, geoUri);
+//                StartActivity(mapIntent);
+//                return;
+//            }
+//
+//            //SampleActivity activity = _activities[position];
+//            //activity.Start(this);
+//        }
+
+//        private void InitializeListView()
+//        {
+//            if (_isGooglePlayServicesInstalled)
+//            {
+////                _activities = new List<SampleActivity>
+////                                  {
+////                                      new SampleActivity(Resource.String.mapsAppText, Resource.String.mapsAppTextDescription, null),
+////                                      new SampleActivity(Resource.String.activity_label_axml, Resource.String.activity_description_axml, typeof(BasicDemoActivity)),
+////                                      new SampleActivity(Resource.String.activity_label_mapwithmarkers, Resource.String.activity_description_mapwithmarkers, typeof(MapWithMarkersActivity)),
+////              	                      new SampleActivity(Resource.String.activity_label_mapwithoverlays, Resource.String.activity_description_mapwithoverlays, typeof(MapWithOverlaysActivity))
+////                                  };
+////
+////                ListAdapter = new SimpleMapDemoActivityAdapter(this, _activities);
+//
+//            }
+//            else
+//            {
+//                Log.Error("MainActivity", "Google Play Services is not installed");
+//                ListAdapter = new SimpleMapDemoActivityAdapter(this, null);
+//            }
+//        }
+//
+        private bool TestIfGooglePlayServicesIsInstalled()
+        {
+			int queryResult = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable (this);
+            if (queryResult == ConnectionResult.Success)
+            {
+                Log.Info(Tag, "Google Play Services is installed on this device.");
+                return true;
+            }
+
+			if (GoogleApiAvailability.Instance.IsUserResolvableError (queryResult))
+            {
+				string errorString = GoogleApiAvailability.Instance.GetErrorString(queryResult);
+                Log.Error(Tag, "There is a problem with Google Play Services on this device: {0} - {1}", queryResult, errorString);
+				Dialog errorDialog = GoogleApiAvailability.Instance.GetErrorDialog (this, queryResult, InstallGooglePlayServicesId);
+                ErrorDialogFragment dialogFrag = new ErrorDialogFragment(errorDialog);
+
+                dialogFrag.Show(FragmentManager, "GooglePlayServicesDialog");
+            }
+            return false;
+        }
+
+		public void initLocationManager()
+		{
+			if (ActivityCompat.CheckSelfPermission(this, Android.Manifest.Permission.AccessCoarseLocation) != (int)Android.Content.PM.Permission.Granted)
+			{
+
+				// CoarseLocation permission has not been granted
+				RequestCoarsePermission();
+			}
+			if (ActivityCompat.CheckSelfPermission(this, Android.Manifest.Permission.AccessFineLocation) != (int)Android.Content.PM.Permission.Granted)
+			{
+
+				// FineLocation permission has not been granted
+				RequestFinePermission();
+			}
+			if (ActivityCompat.CheckSelfPermission(this, Android.Manifest.Permission.Internet) != (int)Android.Content.PM.Permission.Granted)
+			{
+
+				// Internet permission has not been granted
+				RequestInternetPermission();
+			}
+
+			//gps.InitializeLocationManager();
+		}
+
+		/**
+     	* Requests the CoarseLocation permission.
+		* If the permission has been denied previously, a SnackBar will prompt the user to grant the
+		* permission, otherwise it is requested directly.
+		*/
+		void RequestCoarsePermission()
+		{
+			//Log.Info (TAG, "COARSE permission has NOT been granted. Requesting permission.");
+
+			if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Android.Manifest.Permission.AccessCoarseLocation))
+			{
+				// Provide an additional rationale to the user if the permission was not granted
+				// and the user would benefit from additional context for the use of the permission.
+				// For example if the user has previously denied the permission.
+				//Log.Info (TAG, "Displaying COARSE permission rationale to provide additional context.");
+
+				//Snackbar.Make(layout, Resource.String.hello,
+				//	Snackbar.LengthIndefinite).SetAction(Resource.String.hello, new Action<View>(delegate (View obj)
+				//	{
+				//		ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.AccessCoarseLocation }, REQUEST_COARSELOCATION);
+				//	})).Show();
+
+				ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.AccessCoarseLocation }, REQUEST_COARSELOCATION);
+			}
+			else {
+				// Camera permission has not been granted yet. Request it directly.
+				ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.AccessCoarseLocation }, REQUEST_COARSELOCATION);
+			}
+		}
+
+		/**
+     	* Requests the FineLocation permission.
+		* If the permission has been denied previously, a SnackBar will prompt the user to grant the
+		* permission, otherwise it is requested directly.
+		*/
+		void RequestFinePermission()
+		{
+			//Log.Info (TAG, "Fine permission has NOT been granted. Requesting permission.");
+
+			if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Android.Manifest.Permission.AccessFineLocation))
+			{
+				// Provide an additional rationale to the user if the permission was not granted
+				// and the user would benefit from additional context for the use of the permission.
+				// For example if the user has previously denied the permission.
+				//Log.Info (TAG, "Displaying Fine permission rationale to provide additional context.");
+
+				//Snackbar.Make(layout, Resource.String.hello,
+				//	Snackbar.LengthIndefinite).SetAction(Resource.String.hello, new Action<View>(delegate (View obj)
+				//	{
+				//		ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.AccessFineLocation }, REQUEST_FINELOCATION);
+				//	})).Show();
+
+				ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.AccessFineLocation }, REQUEST_FINELOCATION);
+
+			}
+			else {
+				// Camera permission has not been granted yet. Request it directly.
+				ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.AccessFineLocation }, REQUEST_FINELOCATION);
+			}
+		}
+
+		/**
+     	* Requests the Internet permission.
+		* If the permission has been denied previously, a SnackBar will prompt the user to grant the
+		* permission, otherwise it is requested directly.
+		*/
+		void RequestInternetPermission()
+		{
+			//Log.Info (TAG, "Internet permission has NOT been granted. Requesting permission.");
+
+			ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.Internet }, REQUEST_INTERNET);
+
+			if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Android.Manifest.Permission.Internet))
+			{
+				// Provide an additional rationale to the user if the permission was not granted
+				// and the user would benefit from additional context for the use of the permission.
+				// For example if the user has previously denied the permission.
+				//Log.Info (TAG, "Displaying Intenet permission rationale to provide additional context.");
+
+				//Snackbar.Make(layout, Resource.String.hello,
+				//	Snackbar.LengthIndefinite).SetAction(Resource.String.hello, new Action<View>(delegate (View obj)
+				//	{
+				//		ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.Internet }, REQUEST_INTERNET);
+				//	})).Show();
+
+				ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.Internet }, REQUEST_INTERNET);
+			}
+			else {
+				// Camera permission has not been granted yet. Request it directly.
+				ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.Internet }, REQUEST_INTERNET);
+			}
+		}
+    }
+}
